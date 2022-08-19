7b0ef7d497754b8fea263349119a23f2796a3200
g1kwon <spring_shot@naver.com>
2022-08-19 20:34:02 +0900

    Initial commit

---
+++ src/test/java/com/springaop/AopTest.java
@@ -0,0 +1,38 @@
+package com.springaop;
+
+import com.springaop.order.OrderRepository;
+import com.springaop.order.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+public class AopTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void aopInfo() {
+        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success() {
+        orderService.orderIem("itemA");
+    }
+
+    @Test
+    void exception() {
+        Assertions.assertThatThrownBy(() -> orderService.orderIem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+}
