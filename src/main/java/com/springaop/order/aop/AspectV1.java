4ff3afdfe0efc7f24cba8d2101626d4dfc9ce009
g1kwon <spring_shot@naver.com>
2022-08-22 05:45:59 +0900

    springaop 구현 - 시작

---
+++ src/main/java/com/springaop/order/aop/AspectV1.java
@@ -0,0 +1,17 @@
+package com.springaop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Slf4j
+@Aspect
+public class AspectV1 {
+
+    @Around("execution(* com.springaop.order..*(..))")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("[log] {}", joinPoint.getSignature()); //join point 시그니처
+        return joinPoint.proceed();
+    }
+}
--- src/test/java/com/springaop/AopTest.java
+++ src/test/java/com/springaop/AopTest.java
@@ -2,15 +2,18 @@ package com.springaop;
 
 import com.springaop.order.OrderRepository;
 import com.springaop.order.OrderService;
+import com.springaop.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
+@Import(AspectV1.class)
 public class AopTest {
 
     @Autowired
