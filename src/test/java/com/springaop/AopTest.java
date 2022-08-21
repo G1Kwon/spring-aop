b8eb9cef42d1b255cd3437abf898759c1d7e764b
g1kwon <spring_shot@naver.com>
2022-08-22 06:42:03 +0900

    springaop 구현 - add advice

--- src/test/java/com/springaop/AopTest.java
+++ src/test/java/com/springaop/AopTest.java
@@ -4,6 +4,7 @@ import com.springaop.order.OrderRepository;
 import com.springaop.order.OrderService;
 import com.springaop.order.aop.AspectV1;
 import com.springaop.order.aop.AspectV2;
+import com.springaop.order.aop.AspectV3;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -15,7 +16,8 @@ import org.springframework.context.annotation.Import;
 @Slf4j
 @SpringBootTest
 //@Import(AspectV1.class)
-@Import(AspectV2.class)
+//@Import(AspectV2.class)
+@Import(AspectV3.class)
 public class AopTest {
 
     @Autowired
