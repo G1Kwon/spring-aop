40c41aff2719303df509265600bf1c6ac59b98fc
g1kwon <spring_shot@naver.com>
2022-08-28 10:46:46 +0900

    springaop 예제 - 로그 출력 aop

---
+++ src/main/java/com/springaop/exam/ExamService.java
@@ -0,0 +1,17 @@
+package com.springaop.exam;
+
+import com.springaop.exam.annotation.Trace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExamService {
+
+    private final ExamRepository examRepository;
+
+    @Trace
+    public void request(String itemId) {
+        examRepository.save(itemId);
+    }
+}
